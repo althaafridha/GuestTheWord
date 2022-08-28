1bf20b0f6893c9f2d21507b011c391b99766befb
Althaafzz <althaaf.zaydaan@gmail.com>
2022-08-28 14:26:08 +0700

    add LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,14 +1,28 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 	// The current word
-	var word = ""
+	private val _word = MutableLiveData<String>()
+
+	val word: LiveData<String>
+		get() = _word
+
 	// The current score
-	var score = 0
-	// The list of words - the front of the list is the next word to guess
+	private val _score = MutableLiveData<Int>()
+
+	val score: LiveData<Int>// The list of words - the front of the list is the next word to guess
+		get() = _score
+
+	// Event which triggers the end of the game
+	private val _eventGameFinish = MutableLiveData<Boolean>()
+	val eventGameFinish: LiveData<Boolean>
+		get() = _eventGameFinish
+
 	private lateinit var wordList: MutableList<String>
 
 	/**
@@ -42,6 +56,9 @@ class GameViewModel : ViewModel() {
 	}
 
 	init {
+		_word.value = ""
+		_score.value = 0
+
 		resetList()
 		nextWord()
 		Log.i("GameViewModel", "GameViewModel created!")
@@ -50,26 +67,36 @@ class GameViewModel : ViewModel() {
 	 * Moves to the next word in the list
 	 */
 	private fun nextWord() {
-		if (!wordList.isEmpty()) {
-			//Select and remove a word from the list
-			word = wordList.removeAt(0)
+		if (wordList.isEmpty()) {
+			onGameFinish()
+		} else {
+			//Select and remove a _word from the list
+			_word.value = wordList.removeAt(0)
 		}
-//		updateWordText()
-//		updateScoreText()
 	}
+
 	/** Methods for buttons presses **/
 	fun onSkip() {
-		score--
+		_score.value = (score.value)?.minus(1)
 		nextWord()
 	}
 
 	fun onCorrect() {
-		score++
+		_score.value = (score.value)?.plus(1)
 		nextWord()
 	}
 
 	override fun onCleared() {
 		super.onCleared()
 		Log.i("GameViewModel", "GameViewModel destroyed!")
+	}
+
+	/** Method for the game completed event **/
+	fun onGameFinish() {
+		_eventGameFinish.value = true
+	}
+
+	fun onGameFinishComplete() {
+		_eventGameFinish.value = false
 	}
 }
